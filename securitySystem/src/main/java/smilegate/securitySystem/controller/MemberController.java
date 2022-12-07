b395ad06f4eeaebd9298956288712bbf48dd847d
tjdwns4537 <tjdwns4537@nate.com>
2022-12-07 18:52:33 +0900

    회원 등록 기능 구현 후 회원 상세 페이지 띄워주는 로직 구현

--- securitySystem/src/main/java/smilegate/securitySystem/controller/MemberController.java
+++ securitySystem/src/main/java/smilegate/securitySystem/controller/MemberController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import smilegate.securitySystem.domain.Member;
 import smilegate.securitySystem.repository.MemberRepositoryImp;
 import smilegate.securitySystem.repository.MemberRepositoryInterface;
@@ -19,8 +20,23 @@ public class MemberController {
     MemberRepositoryInterface memberRepository = MemberRepositoryImp.getInstance();
 
     @GetMapping("/join")
-    public String JoinPage() {
+    public String joinPage() {
         return "/member/join";
+    }
+
+    @PostMapping("/join")
+    public String joinExecute(
+            @RequestParam String userId,
+            @RequestParam String email,
+            @RequestParam String password1,
+            @RequestParam String userName,
+            @RequestParam String phoneNumber,
+            Model model
+    ) {
+        Member member = new Member(userName, userId, email, password1, phoneNumber);
+        memberRepository.save(member);
+        model.addAttribute("member", member);
+        return "/member/checkMember";
     }
 
     @GetMapping("/login")
@@ -30,12 +46,11 @@ public class MemberController {
 
     @PostMapping("/login")
     public String loginExecute(Model model) {
-        return "index";
+        return "/index";
     }
 
-    @PostMapping("/checkMember")
+    @GetMapping("/checkMember")
     public String checkMemberPage() {
-
         return "/member/checkMember";
     }
 
@@ -46,11 +61,11 @@ public class MemberController {
         return "/member/memberListView";
     }
 
-    @PostConstruct
-    public void tempMember() {
-        memberRepository.save(new Member("성준", "tjdwns1@email.com", "1234", "4537"));
-        memberRepository.save(new Member("정각", "jun@email.com", "2345", "1234"));
-        memberRepository.save(new Member("숭어", "aksk@email.com", "3456", "2345"));
-        memberRepository.save(new Member("성준", "tjdwns2@email.com", "1234", "4537"));
-    }
+//    @PostConstruct
+//    public void tempMember() {
+//        memberRepository.save(new Member("성준", "전사","tjdwns1@email.com", "1234", "4537"));
+//        memberRepository.save(new Member("정각", "누구세요","jun@email.com", "2345", "1234"));
+//        memberRepository.save(new Member("숭어", "부산","aksk@email.com", "3456", "2345"));
+//        memberRepository.save(new Member("성준", "북대생","tjdwns2@email.com", "1234", "4537"));
+//    }
 }
--- securitySystem/src/main/java/smilegate/securitySystem/domain/Member.java
+++ securitySystem/src/main/java/smilegate/securitySystem/domain/Member.java
@@ -7,12 +7,14 @@ import lombok.Setter;
 public class Member {
     private Long id;
     private String name;
+    private String userId;
     private String email;
     private String password;
     private String phoneNumber;
 
-    public Member(String name, String email, String password, String phoneNumber) {
+    public Member(String name, String userId, String email, String password, String phoneNumber) {
         this.name = name;
+        this.userId = userId;
         this.email = email;
         this.password = password;
         this.phoneNumber = phoneNumber;
