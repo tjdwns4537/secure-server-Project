d90f091408f3aff4527f40b4aa960a124a5f299f
tjdwns4537 <tjdwns4537@nate.com>
2022-12-16 13:24:50 +0900

    gititgnore 적용

--- securitySystem/src/test/java/smilegate/securitySystem/service/MemberService/memberServiceImpTest.java
+++ securitySystem/src/test/java/smilegate/securitySystem/service/MemberService/memberServiceImpTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import smilegate.securitySystem.domain.Member;
 import smilegate.securitySystem.repository.MemberRepository.MemberRepositoryInterface;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,13 +20,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class memberServiceImpTest {
 
-    @Autowired
-    MemberServiceInterface memberService;
-
-    @Autowired
-    MemberRepositoryInterface memberRepository;
+    @Autowired MemberServiceInterface memberService;
+    @Autowired MemberRepositoryInterface memberRepository;
+    @Autowired EntityManager em;
 
     @Test
+    @Rollback(value = false)
     void join() {
         Member member = new Member("성준", "qwe@naver.com");
 
@@ -33,7 +35,18 @@ class memberServiceImpTest {
     }
 
     @Test
-    void validateDuplicateMember() {
+    void validateDuplicateMember() throws Exception{
+        Member mem1 = new Member("성준1", "qwe@naver.com");
+        Member mem2 = new Member("성준2", "qwe@naver.com");
+
+        memberService.join(mem1);
+        try{
+            memberService.join(mem2);
+        } catch (IllegalStateException e){
+            return;
+        }
+
+        fail("예외가 발생해야 한다.");
     }
 
     @Test
